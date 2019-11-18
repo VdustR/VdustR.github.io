d2034c5444617a8afe1204ecc5f34b5e32bcefa7
VdustR <vdustr@gmail.com>
2019-11-18 23:09:47 +0800

    build: bump versions

---
+++ src/.vuepress/theme/components/PageEdit.vue
@@ -0,0 +1,17 @@
+<script>
+import format from 'date-fns/format';
+import PageEdit from '@vuepress/theme-default/components/PageEdit';
+
+export default {
+  ...PageEdit,
+  computed: {
+    ...PageEdit.computed,
+    lastUpdated() {
+      return (
+        this.$page.lastUpdated &&
+        format(this.$page.lastUpdated, 'yyyy/MM/dd HH:mm:ss')
+      );
+    },
+  },
+};
+</script>
